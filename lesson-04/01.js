cfa5bb6489b73aa18923eee3344bd70bdfcc71ef
DMDAleksan <s.demid97@gmail.com>
2024-12-25 00:32:52 +0300

    gotov

--- lesson-04/01.js
+++ lesson-04/01.js
@@ -14,11 +14,10 @@ includesElement([1, 2, 3], 2)  // должен вернуть `true`.
 */
 
 function includesElement(array, element) {
-    for (let index = 0; index < array.length; index++) {
-        if (element === array[index]) {
-            return true
-        }else {
-            return false
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] === element) {
+            return  true
         }
     }
+    return false
 }
